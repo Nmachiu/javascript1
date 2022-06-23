e4a36aa77d64951b60fa13ffd3295db4a6cb9d03
Nmachiu <105433455+Nmachiu@users.noreply.github.com>
2022-06-23 15:26:46 +0100

    Add files via upload

---
+++ javascript/ashley.js
@@ -0,0 +1,9 @@
+let name = "Ashley";
+let height = "5'7";
+let country = "Nigeria";
+let bio = `Name: ${name}, Height: ${height}, Country: ${country}`;
+
+console.log(bio);
+document.write(` <h1>${bio}<h1>`)
+alert(bio)
+
